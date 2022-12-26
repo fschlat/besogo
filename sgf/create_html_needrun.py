f108a47abf491cf8246c02d74b5d88b39d893890
Frederic Schlattner <frederic.schlattner@gmail.com>
2022-12-26 23:27:09 +0000

    All commited

---
+++ sgf/create_html_needrun.py
@@ -0,0 +1,54 @@
+"""
+26.12.22
+Frederic Schlattner
+
+This file creates multiple html files for each SGF game from the template.html file.
+"""
+
+import os 
+
+
+# Read in the template HTML file
+with open("template.html", "r") as f:
+    template_html = f.read()
+
+print(template_html)
+
+# Open the text file in read mode
+with open("SGF_list.txt", "r") as g:
+    # Read the contents of the file into a string
+    games = g.read()
+
+# Remove all spaces in textfile
+games = games.replace(' ', '')
+
+
+# Split the string into a list of words, using the comma as the delimiter
+game = games.split(",")
+
+# Print the list of words
+print(game)
+
+# Generate the HTML files
+for i, step in enumerate(game):
+
+    # Create the content for the HTML file by replacing the placeholder in the template
+    html = template_html.replace("file_name",step)
+
+    print(html)
+
+    # Save the HTML content to a new file in the specified directory
+    with open(step, "w") as f:
+        f.write(html)
+    
+
+
+
+
+
+
+
+
+
+
+
