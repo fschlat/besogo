45a9feef383ff57bbeac8cc37b9726495edbf3eb
Frederic Schlattner <frederic.schlattner@gmail.com>
2022-12-27 00:29:10 +0000

    Added comments

--- sgf/create_html_needrun.py
+++ sgf/create_html_needrun.py
@@ -3,6 +3,11 @@
 Frederic Schlattner
 
 This file creates multiple html files for each SGF game from the template.html file.
+
+Remember to add:
+1. SGF files to the games_sgf folder, follow nomenclature.
+2. Add name of the SGF file in the text file SGF_list.txt.
+3. Run this python file to get the html files.
 """
 
 import os 
@@ -11,8 +16,6 @@ import os
 # Read in the template HTML file
 with open("template.html", "r") as f:
     template_html = f.read()
-
-print(template_html)
 
 # Open the text file in read mode
 with open("SGF_list.txt", "r") as g:
